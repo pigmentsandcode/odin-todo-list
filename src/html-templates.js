628130ba38e3d15587999e62eaf4267f5d5496cf
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-05-27 16:11:50 -0500

    Add popup html templates

---
+++ src/html-templates.js
@@ -0,0 +1,88 @@
+function getTodoForm() {
+  return `
+        <form action="#" class="popup-form" autocomplete="off">
+          <div class="popup-input-title popup-input-area">
+            <input
+              class="popup-input"
+              type="text"
+              id="mainTitle"
+              name="mainTitle"
+              placeholder=""
+              required
+            />
+            <label for="mainTitle"><span>Title</span></label>
+          </div>
+          <div class="popup-input-description popup-input-area">
+            <textarea
+              name="description"
+              id="description"
+              class="popup-input"
+              placeholder=""
+            ></textarea>
+            <label for="description"><span>Description</span></label>
+          </div>
+          <div class="popup-input-area popup-input-project">
+            <select name="project" id="project">
+              <option value="default">Default</option>
+            </select>
+            <label for="project"><span>Project</span></label>
+          </div>
+          <div class="popup-input-area popup-input-date">
+            <input type="date" class="popup-input" id="dueDate" name="dueDate" />
+            <label for="dueDate"><span>Due Date</span></label>
+          </div>
+          <div class="popup-input-area popup-input-priority">
+            <select name="priority" id="priority">
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+          <button class="popup-btn btn" type="submit">Save Todo</button>
+        </form>
+    `;
+}
+
+function getProjectForm() {
+  return `
+    <form action="#" class="popup-form" autocomplete="off">
+    <div class="popup-input-title popup-input-area">
+        <input type="text" class="popup-input" id="mainTitle" name="mainTitle" />
+        <label for="mainTitle"><span>Title</span></label>
+    </div>
+    <button class="popup-btn btn" type="submit">Save Project</button>
+    </form>
+    `;
+}
+
+function getConfirmPopup(popupType) {
+  `<div class="popup-body">
+    <p class="popup-text">${confirmTexts[popupType].confirmText}</p>
+    <div class="popup-buttons">
+      <button class="popup-btn btn" type="button">Cancel</button>
+      <button class="popup-btn btn" type="button">${confirmTexts[popupType].buttonText}</button>
+    </div>
+  </div>`;
+}
+
+const confirmTexts = {
+  deleteTodo: {
+    confirmText: "Are you sure you want to permanently delete this todo?",
+    buttonText: "Delete",
+  },
+  deleteProject: {
+    confirmText:
+      "If you delete this project, all todos will be moved to the default project. Are you sure you want to permanently delete this project?",
+    buttonText: "Delete",
+  },
+  clearProjectTasks: {
+    confirmText:
+      "This will clear all todos from this project and send them to the default project. Are you sure you want to do this?",
+    buttonText: "Clear Todos",
+  },
+  deleteCompleted: {
+    confirmText:
+      "This will permanently delete all completed todos in this project. Are you sure you want to do this?",
+    buttonText: "Delete completed",
+  },
+};
